833e2e9a25f7e20c60e08abc9eef32413b7c48c0
DarsheelXtha <batasakash@gmail.com>
2023-01-18 08:22:56 +0545

    final code commit

---
+++ server/router/route.js
@@ -0,0 +1,22 @@
+import React from 'express';
+const router = Router();
+
+import * as controller from '..controllers/appController.js';
+import { registeMail } from '../controllers/mailer.js';
+import Auth, { localVariables } from '../middleware/auth';
+
+
+router.route('/register').post(controller.register);
+router.route('/registerMail').post(registeMail);
+router.route('/authenticate').post(controller.verifyUser, (req,res) => res.end());
+router.route('/login').post(controller.verifyUser, controller.login);
+
+router.route('/user/:username').get(controller.getUser);
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP);
+router.route('/verifyOTP').get(controller.verifyUser, controller.verifyOTP);
+router.route('/createResetSession').get(controller.createResetSession);
+
+router.route('/updateUser').put(Auth, controller.updateUser);
+router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword);
+
+export default router;
